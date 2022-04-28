97a49e6488ae99d8f5add0ef4b3183085742d9ca
psychehose <psychehose@gmail.com>
2022-04-28 17:04:30 +0900

    tutorial2

---
+++ TicTacToe/LoggedIn/LoggedInBuilder.swift
@@ -0,0 +1,53 @@
+//
+//  LoggedInBuilder.swift
+//  TicTacToe
+//
+//  Created by KIM HOSE on 2022/04/28.
+//  Copyright © 2022 Uber. All rights reserved.
+//
+
+import RIBs
+
+protocol LoggedInDependency: Dependency {
+  // TODO: Make sure to convert the variable into lower-camelcase.
+  var LoggedInViewController: LoggedInViewControllable { get }
+  // TODO: Declare the set of dependencies required by this RIB, but won't be
+  // created by this RIB.
+}
+
+final class LoggedInComponent: Component<LoggedInDependency>, OffGameDependency {
+  
+  // TODO: Make sure to convert the variable into lower-camelcase.
+  fileprivate var LoggedInViewController: LoggedInViewControllable {
+    return dependency.LoggedInViewController
+  }
+  
+  // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+}
+
+// MARK: - Builder
+
+protocol LoggedInBuildable: Buildable {
+  func build(withListener listener: LoggedInListener) -> LoggedInRouting
+}
+
+final class LoggedInBuilder: Builder<LoggedInDependency>, LoggedInBuildable {
+  
+  override init(dependency: LoggedInDependency) {
+    super.init(dependency: dependency)
+  }
+  
+  func build(withListener listener: LoggedInListener) -> LoggedInRouting {
+    let component = LoggedInComponent(dependency: dependency)
+    let interactor = LoggedInInteractor()
+    interactor.listener = listener
+
+    let offGameBuilder = OffGameBuilder(dependency: component)
+
+    return LoggedInRouter(
+      interactor: interactor,
+      viewController: component.LoggedInViewController,
+      offGameBuilder: offGameBuilder
+    )
+  }
+}
---
+++ TicTacToe/LoggedIn/LoggedInRouter.swift
@@ -0,0 +1,58 @@
+//
+//  LoggedInRouter.swift
+//  TicTacToe
+//
+//  Created by KIM HOSE on 2022/04/28.
+//  Copyright © 2022 Uber. All rights reserved.
+//
+
+import RIBs
+
+protocol LoggedInInteractable: Interactable {
+  var router: LoggedInRouting? { get set }
+  var listener: LoggedInListener? { get set }
+}
+
+protocol LoggedInViewControllable: ViewControllable {
+  // TODO: Declare methods the router invokes to manipulate the view hierarchy. Since
+  // this RIB does not own its own view, this protocol is conformed to by one of this
+  // RIB's ancestor RIBs' view.
+}
+
+final class LoggedInRouter: Router<LoggedInInteractable>, LoggedInRouting {
+  
+  // TODO: Constructor inject child builder protocols to allow building children.
+  init(
+    interactor: LoggedInInteractable,
+    viewController: LoggedInViewControllable,
+    offGameBuilder: OffGameBuilder
+  ) {
+    self.viewController = viewController
+    self.offGameBuilder = offGameBuilder
+    super.init(interactor: interactor)
+    interactor.router = self
+  }
+  
+  override func didLoad() {
+    super.didLoad()
+    attachOffGame()
+  }
+  
+  func cleanupViews() {
+    // TODO: Since this router does not own its view, it needs to cleanup the views
+    // it may have added to the view hierarchy, when its interactor is deactivated.
+  }
+  
+  // MARK: - Private
+  
+  private let viewController: LoggedInViewControllable
+  private let offGameBuilder: OffGameBuildable
+  private var currentChild: ViewableRouting?
+  
+  private func attachOffGame() {
+    let offGame = offGameBuilder.build(withListener: interactor)
+    self.currentChild = offGame
+    attachChild(offGame)
+    viewController.present(viewController: offGame.viewControllable)
+  }
+}
